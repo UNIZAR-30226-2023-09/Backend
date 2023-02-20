0d56a7d26f972c29b4fdda4a19f40058d21d1543
davidrseves <davidrseves@gmail.com>
2023-02-20 11:45:14 +0100

    Create drop_tables.sql

---
+++ src/mysql/drop_tables.sql
@@ -0,0 +1,13 @@
+--------------------------------------------------------------------------------
+-- Proyecto Software (2022/2023)                                              --
+-- Fichero: drop_tables.sql                                                   --
+--------------------------------------------------------------------------------
+
+
+DROP TABLE IF EXISTS estaEnTorneo;
+DROP TABLE IF EXISTS juega;
+DROP TABLE IF EXISTS Partida;
+DROP TABLE IF EXISTS Torneo;
+DROP TABLE IF EXISTS tieneSkins;
+DROP TABLE IF EXISTS Jugador;
+DROP TABLE IF EXISTS Skins;
