dd8a5a05d2e18ac882dade7d29d6c54b90dd6739
alejandrolalaguna <alejandrolalaguna@gmail.com>
2023-02-21 10:41:05 +0100

    Modificar y añadir algunas funciones de análisis de mensajes
    Registrarse, iniciar sesión, crear y unirse partida, lanzar dados

---
+++ src/server/funcionesJugador.js
@@ -0,0 +1,12 @@
+/*
+ ---------------------------------------------------------------------------
+ * Fichero: funcionesJugador.js
+ * Autor: César Moro Latorre, Alejandro Lalaguna Maza
+ * NIP: 815078, 819860
+ * Descripción: Fichero de funciones auxiliares correspondientes al jugador
+ ---------------------------------------------------------------------------
+*/
+
+function Registro(email, contrasenya, nombre) {
+    
+}
--- src/server/main.js
+++ src/server/main.js
@@ -32,11 +32,11 @@ server.on('connection', (socket) => {
         let mensaje = message.split(',');
         // TODO: Switch de diferentes mensajes recibidos
         // Si se quiere registrar
-        if (mensaje == 'registrarse') {
+        if (mensaje[0] == 'registrarse') {
             // Descomponer el mensaje
-            let email = mensaje[0];
-            let contrasenya = mensaje[1];
-            let nombre = mensaje[2];
+            let email = mensaje[1];
+            let contrasenya = mensaje[2];
+            let nombre = mensaje[3];
             // Llamar a la función de la api correspondiente para el registro
             // TODO: Ver si se llama así o como
             // Si se ha registrado correctamente
@@ -50,10 +50,10 @@ server.on('connection', (socket) => {
         }
 
         // Si se quiere registrar
-        if (mensaje == 'iniciarSesion') {
+        if (mensaje[0] == 'iniciarSesion') {
             // Descomponer el mensaje
-            let email = mensaje[0];
-            let contrasenya = mensaje[1];
+            let email = mensaje[1];
+            let contrasenya = mensaje[2];
             // Llamar a la función de la api correspondiente para comprobar inicio de sesión
             // Obtenemos el valor devuelto por la función (el num de gemas de ese usuario)
             let gemas = comprobarInicioSesion(email, contrasenya);
@@ -75,13 +75,58 @@ server.on('connection', (socket) => {
             }
         }
 
+        // Si el mensaje es que se ha creado una partida
+        if (mensaje[0] == 'crearPartida') {
+            // Descomponer el mensaje
+            let ID_jugador = mensaje[1];
+            let numJugadores = mensaje[2];
+            // Creamos la partida y guardamos su ID
+            let id_partida = crearPartida(numJugadores);
+            socket.send(`Partida creada con ID: ${id_partida}`);
+        }
+
+        // Si el mensaje es que se ha creado una partida
+        if (mensaje == 'unirsePartida') {
+            // Descomponer el mensaje
+            let ID_partida = mensaje[1];
+            let ID_jugador = mensaje[2];
+            // Unimos al jugador a la partida 
+            if (unirPartida(ID_jugador, ID_partida)) {
+                socket.send(`Jugador unido correctamente a la partida`);
+            }
+            else { // TODO: ¿Motivo?
+                socket.send(`Jugador no unido correctamente`);
+            }
+        }
+
+
+
+
         // Si el mensaje es que se han lanzado los dados
-        if (mensaje === 'lanzarDados') {
+        if (mensaje[0] === 'lanzarDados') {
             let tiradaDados1 = Math.floor(Math.random() % 6) + 1;
             let tiradaDados2 = Math.floor(Math.random() % 6) + 1;
             let sumaDados = tiradaDados1 + tiradaDados2;
-	        // enviar a la base de datos posicion jugador
-            socket.send(`Has sacado un ${sumaDados}`);
+            // TODO: El jugador lo llevamos nosotros o nos lo pasan por mensaje?
+            let posicionNueva = moverJugador(jugador, sumaDados);
+            // Comprobar si la nueva posición corresponde a la casilla de la cárcel
+            if (comprobarCasillaCarcel(posicionNueva)) {
+                socket.send(`El dado 1 es: ${tiradaDados1}`);
+                socket.send(`El dado 2 es: ${tiradaDados2}`);
+                enviarCarcel(jugador);
+                // TODO: Definir la casilla de la carcel
+                //socket.send(`La nueva posicion es: ${casillaCarcel}`);
+            }
+            else {
+                // Comprobar si los dados son dobles
+                if (tiradaDados1 == tiradaDados2) {
+                    // TODO: Decirles que reenvien el mensaje porque es el turno del mismo jugador
+                }
+                // enviar a la base de datos posicion jugador
+                socket.send(`El dado 1 es: ${tiradaDados1}`);
+                socket.send(`El dado 2 es: ${tiradaDados2}`);
+                socket.send(`La nueva posicion es: ${posicionNueva}`);
+            }
         }
 
     });
