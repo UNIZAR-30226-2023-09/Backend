e9a88dfebc499fad45582483d22ad64ae0f32eb2
alejandrolalaguna <alejandrolalaguna@gmail.com>
2023-02-22 12:26:41 +0100

    Realizar la función de lanzarDados y comprobarCasilla

--- src/server/funcionesJuega.js
+++ src/server/funcionesJuega.js
@@ -16,26 +16,85 @@ function LanzarDados(ID_jugador, ID_partida) {
     let posicionNueva = moverJugador(ID_jugador, sumaDados);
     // En función de la nueva casilla -> miramos que hacer
     /* Posibles casos al caer en una casilla:
+     *    - Casilla de salida    -> Sumar 300$ al dinero al dinero del jugador (posicionNueva == casillaSalida)
+     *    - Ha pasado por casilla de salida -> Sumar 200$ al dinero del jugador (posicionNueva - sumaDados <= 0)
      *    - Casilla de la cárcel -> ir a la caŕcel
      *    - Casilla de propiedad -> si es de otro jugador -> pagar
      *                           -> si no es de nadie -> mostrar posibilidad de comprarla
      *                           -> si es del propio jugador -> no hacer nada
-     *    - Casilla de salida    -> Sumar 300$ al dinero al dinero del jugador (posicionNueva == casillaSalida)
-     *    - Ha pasado por casilla de salida -> Sumar 200$ al dinero del jugador (posicionNueva - sumaDados <= 0)
-     *    - Casilla de bote      -> sumarle el dinero del juego
+     *    - Casilla de banco     -> sumarle el dinero del juego
+     *    - Casilla treasure     -> sumarle al jugador el dinero o restarselo y sumarselo al banco
+     *    - Casilla surprise     -> obtener carta
      */
 
-    // Comprobar si la nueva posición corresponde a la casilla de la cárcel
-    if (comprobarCasillaCarcel(posicionNueva)) {
-        socket.send(`El dado 1 es: ${dado1}`);
-        socket.send("El dado 2 es: ${dado2}");
-        enviarCarcel(jugador);
-        // TODO: Definir la casilla de la carcel
-        // posicionNueva = $casillaCarcel;
+    // Comprobar si ha pasado por la casilla de salida en este turno
+    if ((posicionNueva - sumaDados) <= 0) {
+        // Si ha pasado, le sumamos 200$ al jugador
+        modificarDinero(ID_jugador, 200);
     }
+
+    // Comprobar la casilla y realizar la acción oportuna
+    comprobarCasilla(posicionNueva, ID_jugador);
+
     // Enviar la nueva posición del jugador y el valor de los dados
     socket.send("El dado 1 es: ${dado1}");
     socket.send("El dado 2 es: ${dado2}");
     socket.send("La nueva posicion es: ${posicionNueva}");
 }
 exports.LanzarDados = LanzarDados;
+
+
+function comprobarCasilla(posicion, ID_jugador) {
+    let tablero = ["Salida","Monterrey","Guadalajara","Treasure","Tax","Estacion",
+        "Tokio","Kioto","Surprise","Osaka","Carcel","Roma","Milan","Treasure","Napoles",
+        "Estacion","Londres","Surprise","Manchester","Edimburgo","Banco","Madrid",
+        "Barcelona","Treasure","Valencia","Estacion","Paris","Surprise","Marsella",
+        "Lyon","VueltaCarcel","Toronto","Vancouver","Treasure","Ottawa","Estacion",
+        "Nueva York","Los Angeles","LuxuryTax","Chicago"];
+    
+    // Comprobar si la nueva casilla es la de salida -> sumar 300$
+    if (posicion == 1) {
+        modificarDinero(ID_jugador, 300);
+    }
+
+    // Comprobar si la nueva casilla es la del banco
+    if (posicion == 21) {
+        sumarDineroBanco(ID_jugador);
+    }
+
+    // Comprobar si la nueva casilla es la de ir a la cárcel
+    if (posicion == 31) {
+        enviarCarcel(ID_jugador);
+    }
+
+    // Comprobar si es casilla de treasure
+    if (posicion == 4 || posicion == 14 || posicion == 24 || posicion == 34) {
+        // Definir y hacer que hacemos en esta casilla
+    }
+
+    // Comprobar si es casilla de surprise
+    if (posicion == 9 || posicion == 18 || posicion == 28) {
+        // Definir y hacer que hacemos en esta casilla
+    }
+
+    // Si la nueva casilla es la de la cárcel (11) -> no hacer nada
+    if (posicion == 1) {
+        // No se hace nada, se pasa turno y ya
+    }
+
+    // Si no es ninguna de las anteriores -> es casilla de propiedad
+    let IDjugador_propiedad = obtenerJugadorPropiedad(tablero[posicion - 1]);
+
+    // Comprobamos si la propiedad no pertenece a ningún jugador
+    if (IDjugador_propiedad == -1) {
+        // TODO: Dar opción de comprarla
+    }
+    // Comprobamos si la propiedad es de otro jugador -> tiene que pagarle
+    else if (IDjugador_propiedad != ID_jugador) {
+        // Pagar al jugador que posee la propiedad
+        // pagarAlquiler(jugadorPaga, jugadorRecibe, propiedad)
+        pagarAlquiler(ID_jugador, IDjugador_propiedad, tablero[posicion - 1]);
+    }
+    // Si pertenece al propio jugador no se hace nada -> no hace falta comprobarlo
+
+}
