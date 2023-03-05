15a0f0ac928114f5a35f5cbd0d102a725260f34b
davidrseves <davidrseves@gmail.com>
2023-03-05 20:27:19 +0100

    Nuevo programa de administracion de la db y cambio de directorios

---
+++ src/baseDeDatos/exec_remote_sql.js
@@ -0,0 +1,49 @@
+/******************************************************************************\
+* Asignatura: Proyecto Software (2022/2023)                                    *
+* Fichero: exec_remote_sql.js                                                  *
+* Autores: David Rivera Seves (NIP: 815124)                                    *
+* Ejecución: node exec_remote_sql 'SHOW TABLES'                                *
+\******************************************************************************/
+
+const mysql = require('mysql');
+
+// Lee la consulta SQL desde la línea de comandos
+const args = process.argv.slice(2);
+const sqlQuery = args[0];
+
+// Verifica si se ha pasado un argumento
+if (!sqlQuery) {
+  console.error('Debes especificar la consulta SQL a ejecutar');
+  process.exit(1);
+}
+
+// Establece los detalles de conexión a la base de datos
+const connection = mysql.createConnection({
+  host: '34.175.167.234',
+  user: 'root',
+  password: 'psbackend1234',
+  database: 'otterfortune_main_db',
+});
+
+// Conéctate a la base de datos
+connection.connect();
+
+// Ejecuta la consulta SQL en la base de datos
+connection.query(sqlQuery, function (error, results, fields) {
+  if (error) throw error;
+  console.log('La consulta SQL se ha ejecutado correctamente');
+  if (results.length > 0) {
+    console.log('[');
+    // Extrae los valores de los objetos y los imprime en la consola
+    results.forEach((result) => {
+      console.log(Object.values(result)[0]);
+    });
+    console.log(']');
+  } else {
+    console.log('[]');
+  }
+  console.log();
+});
+
+// Cierra la conexión a la base de datos
+connection.end();
--- src/baseDeDatos/sql/create_tables.sql
+++ src/baseDeDatos/sql/create_tables.sql
@@ -1,6 +1,7 @@
 --
 -- Proyecto Software (2022/2023)
 -- Fichero: create_tables.sql
+-- Autor: David Rivera Seves (NIP: 815124)
 --
 
 
