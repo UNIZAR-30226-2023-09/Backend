a06b05ecd961f8c684d00fea227176d8cdab7d81
davidrseves <davidrseves@gmail.com>
2023-03-17 17:16:01 +0100

    Update ejecutarSQL.sh

--- src/baseDeDatos/ejecutarSQL.sh
+++ src/baseDeDatos/ejecutarSQL.sh
@@ -211,59 +211,50 @@ then
 
 	# Poblar tables
 	node exec_remote_sql '''
-	-- Skins
 	INSERT INTO Skins (precioGemas, idSkin) VALUES
-	(0, 'default'),
-	(10, 'skin1'),
-	(15, 'skin2');
+	(0, "default"),
+	(10, "skin1"),
+	(15, "skin2")
 	'''
 	node exec_remote_sql '''
-	-- Jugadores
 	INSERT INTO Jugador (gemas, nombre, pass, email) VALUES
-	(10, 'jugador1', '1234', 'jugador1@example.com'),
-	(20, 'jugador2', '1234', 'jugador2@example.com'),
-	(30, 'jugador3', '1234', 'jugador3@example.com');
+	(10, "jugador1", "1234", "jugador1@example.com"),
+	(20, "jugador2", "1234", "jugador2@example.com"),
+	(30, "jugador3", "1234", "jugador3@example.com")
 	'''
 	node exec_remote_sql '''
-	-- Jugador tiene Skin
 	INSERT INTO tieneSkins (idSkin, email) VALUES
-	('default', 'jugador1@example.com'),
-	('default', 'jugador2@example.com'),
-	('default', 'jugador3@example.com');
+	("default", "jugador1@example.com"),
+	("default", "jugador2@example.com"),
+	("default", "jugador3@example.com")
 	'''
 	node exec_remote_sql '''
-	-- Partida rapida
 	INSERT INTO Partida (ronda, bote, evento, economia, precioBase) VALUES
-	(0, 0.0, 'Inicial', 0.0, 0.0);
+	(0, 0.0, "Inicial", 0.0, 0.0)
 	'''
 	node exec_remote_sql '''
-	-- Juegan
 	INSERT INTO juega (numPropiedades, dineroInvertido, nTurnosCarcel, posicion, dinero, skin, puestoPartida, email, idPartida) VALUES
-	(0, 0.0, 0, 0, 0.0, 'default', 'jugador1@example.com', 1),  -- Realemnte no puede usar dos skins iguales, pero para probar sirve
-	(0, 0.0, 0, 0, 0.0, 'default', 'jugador2@example.com', 1);
+	(0, 0.0, 0, 0, 0.0, "default", "jugador1@example.com", 1),  -- Realemnte no puede usar dos skins iguales, pero para probar sirve
+	(0, 0.0, 0, 0, 0.0, "default", "jugador2@example.com", 1)
 	'''
 	node exec_remote_sql '''
-	-- Torneo
 	INSERT INTO Torneo (nPartidas, estaActivo) VALUES
-	(2, NULL);
+	(2, NULL)
 	'''
 	node exec_remote_sql '''
-	-- Jugadores en este torneo
 	INSERT INTO estaEnTorneo (idTorneo, email) VALUES
-	(1, 'jugador2@example.com'),
-	(1, 'jugador3@example.com');
+	(1, "jugador2@example.com"),
+	(1, "jugador3@example.com")
 	'''
 	node exec_remote_sql '''
-	-- Dos partidas de torneos
 	INSERT INTO Partida (ronda, bote, evento, economia, precioBase, perteneceTorneo) VALUES
-	(0, 0.0, 'Inicial', 0.0, 0.0, 1),
-	(0, 0.0, 'Inicial', 0.0, 0.0, 1);
+	(0, 0.0, "Inicial", 0.0, 0.0, 1),
+	(0, 0.0, "Inicial", 0.0, 0.0, 1)
 	'''
 	node exec_remote_sql '''
-	-- Juegan en la primera partida del torneo, la segunda en esta prueba me da igual
 	INSERT INTO juega (numPropiedades, dineroInvertido, nTurnosCarcel, posicion, dinero, skin, puestoPartida, email, idPartida) VALUES
-	(0, 0.0, 0, 0, 0.0, 'default', 'jugador2@example.com', 2),  -- Realemnte no puede usar dos skins iguales, pero para probar sirve
-	(0, 0.0, 0, 0, 0.0, 'default', 'jugador3@example.com', 2);
+	(0, 0.0, 0, 0, 0.0, "default", "jugador2@example.com", 2),  -- Realemnte no puede usar dos skins iguales, pero para probar sirve
+	(0, 0.0, 0, 0, 0.0, "default", "jugador3@example.com", 2)
 	'''
 
 elif [ $ejecutar -eq 2 ]
