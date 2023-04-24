0b24c969b0a729472ca7b33c5f5946a610314a7f
cmoro2002 <cmoro2002@gmail.com>
2023-04-24 12:25:43 +0200

    Crear los eventos de las partidas

--- src/server/funcionesJugador.js
+++ src/server/funcionesJugador.js
@@ -122,28 +122,167 @@ async function FinTurno(ID_jugador, ID_partida) {
     // Comprobar si es fin de ronda y realizar lo oportuno con esta
     if (finRonda == 1) {
         // TODO: Hacer lo necesario con los eventos, actualizar saldos de los bancos, economía
-        await actualizarDinerosBanco(jugadores_struct, ID_partida);
-
-
+        await actualizarFinRonda(jugadores_struct, ID_partida);
     }
 }
 exports.FinTurno = FinTurno;
 
-async function actualizarDinerosBanco(jugadores_struct, ID_partida) {
+// Función que actualiza los dineros de los bancos y el evento de los jugadores al final de la ronda
+async function actualizarFinRonda(jugadores_struct, ID_partida) {
     for (let i = 0; i < jugadores_struct.length; i++) {
         // Comprobar si el jugador tiene dinero en el banco y si es así, multiplicar 
         // por el interés y actualizar el saldo del banco
         let dineroBanco = await APIpartida.dineroBanco(jugadores_struct[i].id, ID_partida);
         if (dineroBanco > 0) {
             let economia = await APIpartida.obtenerEconomia(ID_partida);
-            let interes = economia * 1.3;
+            let interes = economia * 1.2;
             let dinero = dineroBanco * interes;
             await APIpartida.meterDineroBanco(jugadores_struct[i].id, ID_partida, dinero);
+        }
+
+        // Obtener la ronda en la que esta la partida y si es ronda par y mayor que 10, 
+        // generar evento aleatorio
+        let ronda = await APIpartida.obtenerRonda(ID_partida);
+        if (ronda >= 10 && ronda % 2 === 0) {
+            await generarEventoAleatorio(ID_partida);
+        } else {
+            await APIpartida.actualizarEvento(ID_partida, "Ninguno");
         }
 
     }
 }
 
+// Funcion que dada una partida genera un evento aleatorio entre 1 y 5, almacenandolo 
+// en la base de datos y enviandolo a los jugadores que no son bots
+async function generarEventoAleatorio(ID_partida, jugadores_struct) {
+    let evento = Math.floor(Math.random() * 5) + 1;
+    switch (evento) {
+        case 1:
+            // El banco quiebra y se le quita el dinero a los usuarios
+            await eventoBancaRota(efecto, ID_partida, jugadores_struct);
+            break;
+        case 2:
+            // El banco se dispara y se multiplica el dinero del banco de los 
+            // usuarios por 3
+            await eventoBancoDispara(efecto, ID_partida, jugadores_struct);
+            break;
+        case 3:
+            // Los dados esta ronda se multiplican por 2
+            await eventoDadosDobles(efecto, ID_partida, jugadores_struct);
+            break;
+        case 4:
+            // Los dados esta ronda se dividen por 2
+            await eventoDadosMitad(efecto, ID_partida, jugadores_struct);
+            break;
+        case 5:
+            // La economia se vuelve inestable y puede ser muy alta o muy baja
+            await eventoEconomiaInestable(efecto, ID_partida, jugadores_struct);
+            break;
+    }
+}
+
+// Evento que hace que el banco pierda todo su dinero.
+async function eventoBancaRota(efecto, ID_partida, jugadores_struct) {
+    efecto = "BancaRota";
+    await APIpartida.actualizarEvento(ID_partida, efecto);
+    // Actualizar el dinero de los bancos de todos los jugadores a 0
+    for (let i = 0; i < jugadores_struct.length; i++) {
+        await APIpartida.modificarDineroBanco(ID_partida, jugadores_struct[i].id, 0);
+        if (jugadores_struct[i].esBot === "0") {
+            let conexionUsuario = con.buscarUsuario(jugadores_struct[i].id);
+            if (conexionUsuario === null) {
+                console.log('NO SE ENCUENTRA ESE USUARIO NO BOT');
+            } else {
+                conexionUsuario.send(`EVENTO,${efecto}`);
+                conexionUsuario.send(`ACTUALIZAR_DINERO_BANCO,0`);
+            }
+        }
+    }
+    return efecto;
+}
+
+// Evento que multiplica el dinero del banco de los jugadores por 3
+async function eventoBancoDispara(efecto, ID_partida, jugadores_struct) {
+    efecto = "BancoDispara";
+    await APIpartida.actualizarEvento(ID_partida, efecto);
+    for (let i = 0; i < jugadores_struct.length; i++) {
+        let dineroBanco = await APIpartida.dineroBanco(jugadores_struct[i].id, ID_partida);
+        dineroBanco = dineroBanco * 3;
+        await APIpartida.modificarDineroBanco(ID_partida, jugadores_struct[i].id, dineroBanco);
+        if (jugadores_struct[i].esBot === "0") {
+            let conexionUsuario = con.buscarUsuario(jugadores_struct[i].id);
+            if (conexionUsuario === null) {
+                console.log('NO SE ENCUENTRA ESE USUARIO NO BOT');
+            } else {
+                conexionUsuario.send(`EVENTO,${efecto}`);
+                conexionUsuario.send(`ACTUALIZAR_DINERO_BANCO,${dineroBanco}`);
+            }
+        }
+    }
+    return efecto;
+}
+
+// Evento que multiplica los dados de los jugadores por 2
+async function eventoDadosDobles(efecto, ID_partida, jugadores_struct) {
+    efecto = "DadosDobles";
+    await APIpartida.actualizarEvento(ID_partida, efecto);
+    for (let i = 0; i < jugadores_struct.length; i++) {
+        if (jugadores_struct[i].esBot === "0") {
+            let conexionUsuario = con.buscarUsuario(jugadores_struct[i].id);
+            if (conexionUsuario === null) {
+                console.log('NO SE ENCUENTRA ESE USUARIO NO BOT');
+            } else {
+                conexionUsuario.send(`EVENTO,${efecto}`);
+            }
+        }
+    }
+    return efecto;
+}
+
+// Evento que divide los dados de los jugadores por 2
+async function eventoDadosMitad(efecto, ID_partida, jugadores_struct) {
+    efecto = "DadosMitad";
+    await APIpartida.actualizarEvento(ID_partida, efecto);
+    for (let i = 0; i < jugadores_struct.length; i++) {
+        if (jugadores_struct[i].esBot === "0") {
+            let conexionUsuario = con.buscarUsuario(jugadores_struct[i].id);
+            if (conexionUsuario === null) {
+                console.log('NO SE ENCUENTRA ESE USUARIO NO BOT');
+            } else {
+                conexionUsuario.send(`EVENTO,${efecto}`);
+            }
+        }
+    }
+    return efecto;
+}
+
+// Evento que hace que la economia sea inestable y puede ser muy alta o muy baja
+async function eventoEconomiaInestable(efecto, ID_partida, jugadores_struct) {
+    efecto = "EconomiaInestable";
+    await APIpartida.actualizarEvento(ID_partida, efecto);
+    // La economía será o 0.5 o 2
+    let economia = Math.floor(Math.random() * 2) + 1;
+    if (economia === 1) {
+        economia = 0.5;
+    } else {
+        economia = 2;
+    }
+    await APIpartida.actualizarEconomia(ID_partida, economia);
+    for (let i = 0; i < jugadores_struct.length; i++) {
+        if (jugadores_struct[i].esBot === "0") {
+            let conexionUsuario = con.buscarUsuario(jugadores_struct[i].id);
+            if (conexionUsuario === null) {
+                console.log('NO SE ENCUENTRA ESE USUARIO NO BOT');
+            } else {
+                conexionUsuario.send(`EVENTO,${efecto}`);
+                conexionUsuario.send(`ECONOMIA,${economia}`);
+            }
+        }
+    }
+    return efecto;
+}
+
+// Función que devuelve un array con los jugadores de la partida y si son bots o no
 async function obtenerJugadoresPartida(ID_partida) {
     let jugadores = await APIpartida.obtenerJugadoresPartida(ID_partida);
     let jugadoresPartida = jugadores.split(",");
@@ -164,6 +303,8 @@ function Usuario(id, esBot) {
 
 // Escribe en el archivo logs.txt el mensaje que se le pasa.
 function escribirEnArchivo(datos) {
+    // Añadir al archivo logs.txt el mensaje que se le pasa junto al dia y la hora actual
+    datos = new Date().toLocaleString() + datos + " " + "\n";
     fs.writeFile("logs.txt", datos, (error) => {
         if (error) {
             console.error(`Error al escribir en el archivo logs.txt: ${error}`);
