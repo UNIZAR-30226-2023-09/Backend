43824f9137a5aada41dc8d03df6b05d94f8cb039
davidrseves <davidrseves@gmail.com>
2023-04-28 00:49:50 +0200

    Añadir Despliegue

---
+++ src/baseDeDatos/administracion/server_definitivo/despliegue/backend/setup_containers.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+###########################################################################################################
+# Fichero: setup_containers.sh                                                                            #
+# Autor: David Rivera Seves (NIP: 815124)                                                                 #
+# Descripción: Este script sirve para instalar y configurar Docker, Docker-Compose y Git,                 #
+#              y para poner en marcha los contenedores de una aplicación de backend y                     #
+#              frontend web que utilizan React, Node MySQL.                                               #
+#                                                                                                         #
+# Ejecución:                                                                                              #
+#   - bash setup_containers.sh install: Para instalar Docker, Docker-Compose y Git.                       #
+#   - bash setup_containers.sh start:   Para descargar repositorios y poner en marcha los contenedores.   #
+#   - bash setup_containers.sh stop:    Para finalizar el funcionamiento de los contenedores.             #
+#   - bash setup_containers.sh restart: Para reiniciar los contenedores.                                  #
+#   - bash setup_containers.sh status:  Para consultar es estado del servidor node                        #
+#   - bash setup_containers.sh clear:   Para eliminar los datos de la base de datos mysql                 #
+#                                                                                                         #
+# Entrar en los contenedores:                                                                             #
+#   - MYSQL: docker exec -it db_mysql_psoft bash                                                          #
+#   - NODE: docker exec -it backend_node_psoft bash                                                       #
+#                                                                                                         #
+# Comprobar servicios:                                                                                    #
+#   - MYSQL                                                                                               #
+#      · cmd: docker exec -it db_mysql_psoft mysql -h db -u root -p      (pass: psbackend1234)            #
+#   - NODE <-> MYSQL                                                                                      #
+#      · cmd: docker exec -it backend_node_psoft node src/baseDeDatos/administracion/exec_remote_sql.js 'SHOW TABLES'
+#                                                                                                         #
+###########################################################################################################
+
+
+#
+# Función para instalar y configurar Docker, Docker-Compose y Git
+#
+function install_docker {
+    # Instalar y configurar Docker, Docker-Compose y Git ...
+    sudo apt update
+    sudo apt install -y docker.io docker-compose git-all
+    sudo systemctl enable docker
+    sudo systemctl start docker
+    sudo usermod -aG docker $USER
+    echo; echo "Cierre sesión y vuelva a entrar"; echo
+}
+
+
+#
+# Función para descargar repositorios y poner en marcha los contenedores
+#
+function start_containers {
+    # Descargar repositorios
+    sudo rm -rf $HOME/Backend
+    git clone https://github.com/UNIZAR-30226-2023-09/Backend.git
+
+    sleep 2
+
+    # Puesta en marcha de contenedores para la DB MYSQL y para el Backend NODE
+    docker-compose up -d
+
+    sleep 2
+
+    # Configurar en el contenedor Node
+    docker exec -it backend_node_psoft npm install mysql ws
+    docker exec -d backend_node_psoft node src/server/main.js
+}
+
+
+#
+# Función para finalizar los contentedores
+#
+function stop_containers {
+    # Finalizar funcionamiento de contenedores
+    docker-compose down
+}
+
+
+#
+# Función para reiniciar los contentedores
+#
+function restart_containers {
+    stop_containers
+    sleep 2
+    start_containers
+}
+
+
+#
+# Función para ver si esta activo el servidor node (main.js)
+#
+function status_node {
+  if pgrep -f "main.js" > /dev/null; then
+    echo "El servidor node (main.js) está en ejecución"
+  else
+    echo "El servidor node (main.js) no está en ejecución"
+  fi
+}
+
+
+#
+# Función para eliminar la base de datos mysql
+#
+function clear_mysql {
+  docker volume rm psoftserver_my-db
+}
+
+
+# Validar argumentos
+if [ "$1" == "install" ]; then
+  install_docker
+elif [ "$1" == "start" ]; then
+  start_containers
+elif [ "$1" == "stop" ]; then
+  stop_containers
+elif [ "$1" == "restart" ]; then
+  restart_containers
+elif [ "$1" == "status" ]; then
+  status_node
+elif [ "$1" == "clear" ]; then
+  clear_mysql
+else
+  # Argumento inválido
+  echo "Argumento inválido. Usa 'install' o 'start' o 'stop' o 'restart' o 'status' o 'clear'."
+  exit 1
+fi
+
+# IP: 34.175.149.140
---
+++ src/baseDeDatos/administracion/server_definitivo/despliegue/frontend/setup_containers.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+###########################################################################################################
+# Fichero: setup_containers.sh                                                                            #
+# Autor: David Rivera Seves (NIP: 815124)                                                                 #
+# Descripción: Este script sirve para instalar y configurar Docker, Docker-Compose y Git,                 #
+#              y para poner en marcha los contenedores de una aplicación de backend y                     #
+#              frontend web que utilizan React, Node MySQL.                                               #
+#                                                                                                         #
+# Ejecución:                                                                                              #
+#   - bash setup_containers.sh install: Para instalar Docker, Docker-Compose y Git.                       #
+#   - bash setup_containers.sh start:   Para descargar repositorios y poner en marcha los contenedores.   #
+#   - bash setup_containers.sh stop:    Para finalizar el funcionamiento de los contenedores.             #
+#   - bash setup_containers.sh restart: Para reiniciar los contenedores.                                  #
+#   - bash setup_containers.sh status:  Para consultar el estado del servidor react                       #
+#                                                                                                         #
+# Entrar en los contenedores:                                                                             #
+#   - REACT: docker exec -it frontend_react_psoft bash                                                    #
+#                                                                                                         #
+# Comprobar servicios:                                                                                    #
+#   - REACT                                                                                               #
+#      · cmd: curl http://34.175.172.53:80                                                                #
+#                                                                                                         #
+###########################################################################################################
+
+
+#
+# Función para instalar y configurar Docker, Docker-Compose y Git
+#
+function install_docker {
+    # Instalar y configurar Docker, Docker-Compose y Git ...
+    sudo apt update
+    sudo apt install -y docker.io docker-compose git-all
+    sudo systemctl enable docker
+    sudo systemctl start docker
+    sudo usermod -aG docker $USER
+    systemctl stop apache2
+    sudo systemctl disable apache2
+    echo; echo "Cierre sesión y vuelva a entrar"; echo
+}
+
+
+#
+# Función para descargar repositorios y poner en marcha los contenedores
+#
+function start_containers {
+    # Descargar repositorios
+    sudo rm -rf $HOME/Frontend-Web
+    git clone https://github.com/UNIZAR-30226-2023-09/Frontend-Web.git
+
+    sleep 2
+
+    # Puesta en marcha de contenedores para la DB MYSQL y para el Backend NODE
+    docker-compose up -d
+
+    sleep 2
+
+    # Configurar en el contenedor React
+    docker exec -it frontend_react_psoft npm install
+    docker exec -d frontend_react_psoft npm start
+}
+
+
+#
+# Función para finalizar los contentedores
+#
+function stop_containers {
+    # Finalizar funcionamiento de contenedores
+    docker-compose down
+}
+
+
+#
+# Función para reiniciar los contentedores
+#
+function restart_containers {
+    stop_containers
+    sleep 2
+    start_containers
+}
+
+
+#
+# Función para ver si está activo el servidor React (npm start)
+#
+function status_react {
+  if pgrep -f "react-scripts start" > /dev/null; then
+    echo "El servidor React está en ejecución"
+  else
+    echo "El servidor React no está en ejecución"
+  fi
+}
+
+
+# Validar argumentos
+if [ "$1" == "install" ]; then
+  install_docker
+elif [ "$1" == "start" ]; then
+  start_containers
+elif [ "$1" == "stop" ]; then
+  stop_containers
+elif [ "$1" == "restart" ]; then
+  restart_containers
+elif [ "$1" == "status" ]; then
+  status_react
+else
+  # Argumento inválido
+  echo "Argumento inválido. Usa 'install' o 'start' o 'stop' o 'restart' o 'status'."
+  exit 1
+fi
+
+# Ver quien esta usando el puerto 80 (http)
+# sudo lsof -i :80
+# IP: 34.175.172.53
