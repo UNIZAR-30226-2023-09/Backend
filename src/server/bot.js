e0914b5210ba2cb62f9e88d62e4b936f8862c187
cmoro2002 <cmoro2002@gmail.com>
2023-05-06 16:29:27 +0200

    Añadir retardo en los bots

--- src/server/bot.js
+++ src/server/bot.js
@@ -69,7 +69,8 @@ async function calcularSumaDados(IDpartida, dado1, dado2) {
 // Funcion que automatiza la jugada de un bot
 async function jugar(IDusuario, IDpartida) {
     sigueVivo = true;
-
+    // Esperar 3 segundos
+    await new Promise(resolve => setTimeout(resolve, 3000));
     try {
         let { dado1, dado2, posicionNueva, estaCarcel, sumaDados } = await moverBot(IDusuario, IDpartida);
         let dadosDobles = (dado1 === dado2);
