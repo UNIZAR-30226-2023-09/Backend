b426ed40b99a29cbe06910e8a2ac6fe119d60fca
cmoro2002 <cmoro2002@gmail.com>
2023-05-14 17:37:20 +0200

    SuperPoder 7

--- src/server/funcionesTablero.js
+++ src/server/funcionesTablero.js
@@ -408,8 +408,8 @@ async function GestionTreasure(ID_partida, ID_jugador, socket) {
 }
 
 async function GestionSuperPoder(socket, ID_jugador, ID_partida, posicion) {
-    let superPoder = Math.ceil(Math.random() * 12);
-    // let superPoder = 1;
+    // let superPoder = Math.ceil(Math.random() * 12);
+    let superPoder = 7;
     let nuevaPosicion;
     socket.send(`SUPERPODER,${superPoder}`);
     switch (superPoder) {
